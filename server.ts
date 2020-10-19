2cf45989fcd49e67735564677ac49b5958ed9261
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-19 13:30:37 +0200

    [add] node hello world

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log('ts works!')
+import express from 'express'
+
+
+const app: express.Application = express()
+
+app.get('/', (req, res) => {
+    res.status(234).send('Hello World!')
+})
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
