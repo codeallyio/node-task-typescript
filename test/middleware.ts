5491f79c9934db3ae5a02e7764aac366372a7a45
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-20 16:08:54 +0200

    [add] skeleton for tests

---
+++ test/middleware.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import 'mocha'
+
+
+function hello() {
+    return 'Hello World!';
+}
+
+describe('Hello function', () => {
+    it('should return hello world', () => {
+        const result = hello();
+        expect(result).to.equal('Hello World!');
+    });
+});
