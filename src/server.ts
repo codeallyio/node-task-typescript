e12a091cbfd39cb042dcc0dc958581c8e68b09e2
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-20 16:46:47 +0200

    [add] tests for 2 default middlewares -> checking if user hasn't destroyed stuff

--- src/server.ts
+++ src/server.ts
@@ -34,3 +34,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
+
+export default app
--- test/middleware.ts
+++ test/middleware.ts
@@ -1,14 +1,38 @@
+import chai from 'chai'
 import { expect } from 'chai'
+import chaiHttp from 'chai-http'
 import 'mocha'
 
+import server from '../src/server'
 
-function hello() {
-    return 'Hello World!';
-}
+const should = chai.should()
 
-describe('Hello function', () => {
-    it('should return hello world', () => {
-        const result = hello();
-        expect(result).to.equal('Hello World!');
-    });
-});
+chai.use(chaiHttp)
+
+describe('Server HTTP tests', () => {
+    describe('GET /', () => {
+        it('it should get \'Hello World!\' response', (done) => {
+            chai.request(server)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.text.should.be.a('string').eql('Hello World!')
+                    done();
+                })
+        })
+    })
+
+    describe('GET /get-number', () => {
+        it('it should receive number between 0 and 99', (done) => {
+            chai.request(server)
+                .get('/get-number')
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('drawn')
+                    res.body.drawn.should.be.a('number')
+                    done();
+                })
+        })
+    })
+})
