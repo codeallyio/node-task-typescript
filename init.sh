3c184492b7419064bfe195bc657879ce7075b996
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-23 19:05:08 +0200

    [fix] working: clear port process if in usage

--- init.sh
+++ init.sh
@@ -1,13 +1,8 @@
 # clear port 3000 if is in use
 
-if sudo lsof -t -i:3000
+if sudo ss -tulpn | grep ':3000'
 then
-    echo "Dzień dobry, zastałem Jolkę?"
+    echo "..."
     # killing the process
     fuser -n tcp -k 3000
-fi
-
-if ! sudo lsof -t -i:3000
-then
-    echo "Co pana na codzień najbardziej denerwuje?"
 fi
